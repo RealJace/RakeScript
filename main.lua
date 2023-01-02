d76fb88bb8693041e78524befe004ae25ede0897
RealJace <86373768+RealJace@users.noreply.github.com>
2023-01-02 16:44:01 +0100

    Update main.lua

--- main.lua
+++ main.lua
@@ -4,6 +4,14 @@ repeat task.wait() until game:IsLoaded()
 
 local player = game:GetService("Players").LocalPlayer
 
+local camera = workspace.Camera
+local currentCamera = game:GetService("Workspace").CurrentCamera
+
+local worldToViewportPoint = currentCamera.worldToViewportPoint
+
+local headOff = Vector3.new(0,0.5,0)
+local legOff = Vector3.new(0,3,0)
+
 local RunService = game:GetService("RunService")
 
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
@@ -51,12 +59,44 @@ local function addCharEsp(character,color)
 	end)
 end
 
-RunService.Heartbeat:Connect(function()
+RunService.RenderStepped:Connect(function()
 	for _,player in ipairs(game:GetService("Players"):GetPlayers()) do
 		if player.Character then
-				local esp = player.Character:FindFirstChild("ESP")
+				local hrp = player.Character:FindFirstChild("HumanoidRootPart")
+				local head = player.Character:FindFirstChild("Head")
+				local esp = player.Character:FindFirstChild("ESP",true)
 				if esp then
 					esp.Enabled = esps.players
+				end
+				if esps.players then
+					local boxOutline = Drawing.new("Square")
+					boxOutline.Visible = false
+					boxOutline.Color = Color3.new(0,0,0)
+					boxOutline.Thickness = 3
+					boxOutline.Transparency = 1
+					boxOutline.Filled = false
+					local box = Drawing.new("Square")
+					box.Visible = false
+					box.Color = Color3.new(1,1,1)
+					box.Thickness = 1
+					box.Transparency = 1
+					box.Filled = false
+					if hrp and head then
+						local pos1,onScreen = camera:worldToViewportPoint(hrp.Position)
+						
+						local rootPos,rootV = worldToViewportPoint(currentCamera,hrp.Position)
+						local headPos = worldToViewportPoint(currentCamera,head.Position + headOff)
+						local legPos = worldToViewportPoint(currentCamera,hrp.Position - legOff
+						
+						boxOutline.Visible = onScreen
+						box.Visible = onScreen
+						if onScreen then
+							boxOutline.Size = Vector2.new(1000 / rootPos.Z,headPos.Y - legPos.Y)
+							boxOutline.Position = Vector2.new(rootPos.X - boxOutline.Size.X / 2,rootPos.Y - boxOutline.Size.Y / 2)
+							box.Size = boxOutline.Size
+							box.Position = boxOutline.Position
+						end
+					end
 				end
 		end
 	end
